8e9b0cb6ae0fd2e6e006b70c746ba9f6c58a882b
Arthaud <arthaudp33@gmail.com>
2026-08-31 11:24:02 +0200

    Config for html, css, js from mdn doc

--- serve/config.js
+++ serve/config.js
@@ -4,10 +4,13 @@ import { fileURLToPath } from "url";
 export const ROOT = join(dirname(fileURLToPath(import.meta.url)), "..");
 
 export const SEARCH_DIRS = [
-  { label: "Laravel",  path: "docs/laravel",              exts: [".md"] },
-  { label: "Filament", path: "docs/filament/docs",        exts: [".md"] },
-  { label: "Filament", path: "docs/filament/packages",    exts: [".md"] },
-  { label: "Tailwind", path: "docs/tailwindcss/src/docs", exts: [".mdx"] },
-  { label: "MDN",      path: "docs/mdn/files/en-us",      exts: [".md"] },
-  { label: "PHP",      path: "docs/php",                  exts: [".html"] },
+    { label: "Laravel", path: "docs/laravel", exts: [".md"] },
+    { label: "Filament", path: "docs/filament/docs", exts: [".md"] },
+    { label: "Filament", path: "docs/filament/packages", exts: [".md"] },
+    { label: "Tailwind", path: "docs/tailwindcss/src/docs", exts: [".mdx"] },
+    { label: "PHP", path: "docs/php", exts: [".md"] },
+    { label: "JS", path: "docs/mdn/files/en-us/web/javascript", exts: [".md"] },
+    { label: "CSS", path: "docs/mdn/files/en-us/web/css", exts: [".md"] },
+    { label: "HTML", path: "docs/mdn/files/en-us/web/html", exts: [".md"] },
+    { label: "Web", path: "docs/mdn/files/en-us/web/", exts: [".md"] },
 ];
